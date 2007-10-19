a69d055dea6962973d10e725b49484540427141c
hardaker <hardaker@f775e177-e73a-0410-81f1-ff96b6859a2d>
2007-10-19 03:32:47 +0000

    a prompt step to input parameters
    git-svn-id: https://makerelease.svn.sourceforge.net/svnroot/makerelease/trunk@19 f775e177-e73a-0410-81f1-ff96b6859a2d

---
+++ Makerelease/Step/Prompt.pm
@@ -0,0 +1,34 @@
+package Makerelease::Step::Prompt;
+
+use strict;
+use Makerelease::Step;
+use IO::File;
+
+our @ISA=qw(Makerelease::Step);
+
+sub step {
+    my ($self, $step, $parentstep, $counter) = @_;
+
+    my $done = 0;
+    my $answer;
+    while (!$done) {
+	$done = 1;
+	$answer = $self->getinput($step->{'prompt'});
+	if ($step->{'values'} && $answer !~ $step->{'values'}) {
+	    $self->output("Illegal value; must match: $step->{'values'}\n");
+	    $done = 0;
+	}
+    }
+    $self->{'parameters'}{$step->{'parameter'}} = $answer;
+}
+
+sub document_step {
+    my ($self, $step, $parentstep, $counter) = @_;
+
+    $self->output("Decide on a value for parameter '$step->{parameter}'\n");
+    $self->output("  prompt: $step->{parameter}\n");
+    $self->output("  legal:  $step->{values}\n") if ($self->{'values'});
+}
+
+1;
+
